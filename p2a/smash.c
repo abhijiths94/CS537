0d310ff8501c5b1eeb8f59c328c748901710bfea
Abhijith Somashekhar <abhijith@royal-23.cs.wisc.edu>
2020-02-14 19:29:35 -0600

    Adding working files

--- p2a/smash.c
+++ p2a/smash.c
@@ -65,12 +65,12 @@ void tokenize(char *inp, int * cnt, char** inst, char * delim)
             MEMCHECK(inst[*cnt-1]); 
             strcpy(inst[*cnt -1], pch);
 
-            printf("Tokenize : %s : %s : %ld : %d  \n", delim, pch, strlen(pch), *cnt);
+            //printf("Tokenize : %s : %s : %ld : %d  \n", delim, pch, strlen(pch), *cnt);
         }
         pch = strsep(&inp, delim);
 
     }
-    printf("No of %s inst = %d\n",delim, *cnt);
+    //printf("No of %s inst = %d\n",delim, *cnt);
 }
 
 int check_system_cmd(int argc, char ** argv, char * path_str)
@@ -88,80 +88,15 @@ int check_system_cmd(int argc, char ** argv, char * path_str)
         strcat(path_str, "/");
         strcat(path_str, argv[0]);
 
-        printf("Searching in %s \n", path_str);
+        //printf("Searching in %s \n", path_str);
         access_ret  = access(path_str, X_OK);
         if(access_ret == 0)
         {
-            printf("Found !\n");
+           // printf("Found !\n");
             break;
         }
     }
     return access_ret;
-}
-
-int execute_inst(char *inst)
-{
-    int argc = 0;
-    char ** argv  = (char**) malloc(MAX_INST_SIZE * sizeof(char**));
-    tokenize(inst, &argc, argv, " ");
-
-   exit(0);
-}
-
-int execute_seq_inst(char * inst)
-{
-    char ** seq_inst = (char**) malloc(MAX_SEQ_INST * sizeof(char**));
-    int seq_cnt = 0;
-    int i ;
-
-    tokenize(inst , &seq_cnt, seq_inst, ";");
-    
-    for(i = 0; i < seq_cnt; i++)
-    {
-        execute_inst(seq_inst[i]);
-        printf("\tlen : %lu Executing: %s\n",strlen(seq_inst[i]), seq_inst[i]);
-    }
-
-
-    exit(0);
-}
-
-int execute_par_inst(char **inst, int count )
-{
-    int i, waitrc;
-    int pid[100];
-
-    printf("Creating %d threads ... \n", count);
-    for(i = 0; i < count; i++)
-    {
-        
-        pid[i] = fork();
-        if(pid[i] == 0)
-        {
-            //spawn out child and it will call execv
-            printf("Process %d ..\n", i);
-            sleep(i);
-            execute_seq_inst(inst[i]);
-
-            printf("Error !!!!! Reached point for process %d\n", i);
-        }
-        else if (pid[i] < 0)
-        {
-            printf(" !!!!!!!!! Error on %d PID \n", i);
-        }
-    }
-    
-    waitrc = 0;
-
-    //parent waits for children to finish
-    for(i = 0; i < count; i++)
-    {
-        waitrc = waitpid(pid[i], NULL, 0);
-        printf("------------------------------------- main waitrc %d : %d\n ", i, waitrc);
-    }
-
-    printf("All threads completed \n");
-    return waitrc;
 }
 
 int check_cmd( char * inp, char ** argv, int * argc_p, char * path_str, char * redirect_file, int* redirect)
@@ -177,11 +112,10 @@ int check_cmd( char * inp, char ** argv, int * argc_p, char * path_str, char * r
     char* pos = strchr(inp, '>');
     char * inp_cmd, *inp_file, *inp_file_trm ;
 
-
     /* check redirection syntax */
     if(pos != NULL)
     {
-        printf("Found redirect !!! \n");
+       // printf("Found redirect !!! \n");
         if(strchr(pos + 1 , '>') != NULL)
         {
             // 2 redirection in a single command
@@ -197,19 +131,21 @@ int check_cmd( char * inp, char ** argv, int * argc_p, char * path_str, char * r
 
         strncpy(inp_cmd, inp, (pos-inp));
         strcpy(inp_file, pos+1);
+        inp_file_trm = trim_space(inp_file);
 
-        printf(" CMD : %s\n FILE : %s\n", inp_cmd, inp_file);
-    }
-
-    if(*redirect)
-    {
         /* redirect specific syntax check */
         tokenize(inp_cmd, argc_p, argv, " ");
         argc = *argc_p;
-        ret = check_system_cmd(argc, argv, path_str); // call access function to check since it is not an inbuilt command
-        printf("Path_str after check_system_cmd : %s\n", path_str);
        
-        inp_file_trm = trim_space(inp_file);
+        if(strlen(inp_file_trm) == 0 || argc == 0 || (strchr(inp_file_trm, ' ') != NULL))
+        {
+            ret = -1;
+            return ret;
+        }
+
+        ret = check_system_cmd(argc, argv, path_str); // call access function to check since it is not an inbuilt command
+        //printf("Path_str after check_system_cmd : %s\n", path_str);
+       
         strcpy(redirect_file, inp_file_trm);
         
         free(inp_cmd);
@@ -270,7 +206,7 @@ int check_cmd( char * inp, char ** argv, int * argc_p, char * path_str, char * r
             else
             {
                 ret = check_system_cmd(argc, argv, path_str); // call access function to check since it is not an inbuilt command
-                printf("Path_str after check_system_cmd : %s\n", path_str);
+               // printf("Path_str after check_system_cmd : %s\n", path_str);
             }
         }
     }
@@ -278,12 +214,14 @@ int check_cmd( char * inp, char ** argv, int * argc_p, char * path_str, char * r
 }
 
 
-int execute_cmd(char ** argv, int argc, char * path_str, char * redirect_file, int redirect)
+int execute_cmd(char ** argv, int argc, char * path_str, char * redirect_file, int redirect, int block)
 {
+    /*
     for(int i = 0; i < argc; i++)
         printf("Arg %d : %s\n", i, argv[i]);
+        */
 
-    printf("path str : %s\n", path_str);
+    //printf("path str : %s\n", path_str);
 
     int fork_ret, ret, i;
     int delete_indx = -1;
@@ -299,6 +237,7 @@ int execute_cmd(char ** argv, int argc, char * path_str, char * redirect_file, i
     else if(strcmp(argv[0], "cd") == 0)
     {
         ret = chdir(argv[1]);
+        return ret;
     }
     else if(strcmp(argv[0], "path") == 0)
     {
@@ -353,11 +292,12 @@ int execute_cmd(char ** argv, int argc, char * path_str, char * redirect_file, i
         {
             if(redirect)
             {
-                printf("Printing in file ...\n");
+               // printf("Printing in file ...\n");
                 fd_out = open(redirect_file, O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU);
                 if(fd_out>0)
                 {
                     dup2(fd_out, STDOUT_FILENO);
+                    dup2(fd_out, STDERR_FILENO);
                 }
                 else
                 {
@@ -366,28 +306,30 @@ int execute_cmd(char ** argv, int argc, char * path_str, char * redirect_file, i
             }
 
             ret = execv(path_str, argv );
-            printf("!!!!!!!!! Execv ret : %d\n", ret);
+            //printf("!!!!!!!!! Execv ret : %d\n", ret);
             exit(0);
             (void) ret;
         }
         else
         {
-           // parent waits till finished executing
-           printf("**** Waiting for child to finish \n");
-           waitpid(fork_ret, NULL, 0);
-           printf("child completed!!!! \n");
+            if(block)
+            {
+                // parent waits till finished executing
+               // printf("**** Waiting for child to finish \n");
+                waitpid(fork_ret, NULL, 0);
+                //printf("child completed!!!! \n");
+            }
         }
     }
-    return 0;
+    return fork_ret;
 }
 
 
 
-int main(int argc, char** argv)
+int shell(FILE* fp, int print_prompt )
 {
     char * inp = NULL;
     size_t inp_len = 1024;
-    int parallel_cnt = 0;
     int i = 0;
     int check_cmd_ret = 0;
     int is_multiline = 0;
@@ -396,12 +338,18 @@ int main(int argc, char** argv)
     int args_count;
     char *redirect_file;
     int redirect = 0;
+    int j,k;
+
+    int par_pid[100];
+    int prev_par_pid_cnt = 0, par_pid_cnt = 0;
+
+
     
     inp = (char *)calloc(inp_len*sizeof(char), 1);
     
-    char** par_inst = (char **)malloc(MAX_PAR_INST * sizeof(char**));
     char* inp_dup ;
     char * path_str = NULL;
+
 
     PATH = (char**)malloc(MAX_PATH_CNT * sizeof(char**));  
     args = (char**) malloc(MAX_INST_SIZE * sizeof(char**));
@@ -409,13 +357,50 @@ int main(int argc, char** argv)
     path_str = (char*)calloc(MAX_PATH_LEN * sizeof(char) , 1);
     MEMCHECK(path_str);
 
+    char ** seq_path_str = (char**)malloc(MAX_SEQ_INST * sizeof(char*));
+    MEMCHECK(seq_path_str);
+
+    char ** seq_inst = (char**) malloc(MAX_SEQ_INST * sizeof(char**));
+    int seq_cnt = 0;
+    MEMCHECK(seq_inst);
+
+    char *** seq_args = (char***)malloc(MAX_SEQ_INST * sizeof(char**));
+    MEMCHECK(seq_args);
+
+    int * seq_args_count = (int*)malloc(MAX_SEQ_INST * sizeof(int));
+    MEMCHECK(seq_args_count);
+
+    char ** seq_redirect_file = (char**)malloc(MAX_SEQ_INST * sizeof(char*));
+    MEMCHECK(seq_redirect_file);
+
+    int * seq_redirect = (int*)malloc(MAX_SEQ_INST * sizeof(int));
+    MEMCHECK(seq_redirect);
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//
+    char * multi_mode = (char*)malloc(MAX_SEQ_INST * 20);
+    MEMCHECK(multi_mode);
+
+
+
+    char * par_dup = (char*)malloc(MAX_INST_SIZE);
+    MEMCHECK(par_dup);
+
+    int par_cnt;
+
+    char ** par_inst =(char**) malloc(MAX_PAR_INST * sizeof(char**));
+    MEMCHECK(par_inst);
+    
+
+
+
     /* Memory allocation failed */
-    if(inp == NULL || par_inst == NULL || PATH == NULL)
+    if(inp == NULL || PATH == NULL)
     {
         exit(1);
     }
     
-
     /* Load default PATH */
     PATH[path_cnt] = strdup("/bin");
     MEMCHECK(PATH[path_cnt]);
@@ -423,6 +408,9 @@ int main(int argc, char** argv)
 
     /* Redirect file */
     redirect_file = (char*)malloc(MAX_FILE_LEN);
+    MEMCHECK(redirect_file);
+
+
     
     while(1)
     {
@@ -430,10 +418,21 @@ int main(int argc, char** argv)
         {
             break;
         }
-        printf("smash> ");
-        getline(&inp, &inp_len, stdin);
+
+        if(print_prompt)
+        {
+            printf("smash> ");
+            fflush(stdout);
+        }
+
+
+        if(getline(&inp, &inp_len, fp) == -1 || inp[0] == EOF)
+        {
+            exit(0);
+        }
 
         memset(path_str, 0, strlen(path_str));
+        k = 0;
 
         if(strlen(inp) > 1)
         {
@@ -451,43 +450,181 @@ int main(int argc, char** argv)
             }
             else
             {
-                /* Handle Multiline commands */
-                printf("--------------------------------------------------------\n\n");
-                tokenize(inp_dup, &parallel_cnt, par_inst, "&");
-                
-                printf("Multicommand detected : %d\n", parallel_cnt);
-                printf(" Inp : %s \n ", inp_dup);
+                /* Split Multiline commands by sequential commands */
+                tokenize(inp, &seq_cnt, seq_inst, ";");
+               // printf("Multicommand detected : %d\n", seq_cnt);
+               // printf(" Inp : %s \n ", inp);
 
-                for(i = 0; i < parallel_cnt; i++)
+                for(i = 0, j= 0; i < seq_cnt; i++, j++)
                 {
-                    printf("len : %lu %s\n",strlen(par_inst[i]), par_inst[i]);
+                    if(strchr(seq_inst[j], '&') != NULL)
+                    {
+                        par_cnt = 0;
+                        /* This is a parallel command */
+                        strcpy(par_dup, seq_inst[j]);
+                        tokenize(par_dup, &par_cnt, par_inst, "&");
+
+                        for(k= i; k < i + par_cnt; k++)
+                        {
+                            
+                            //printf("-------------------------> Par inst %d : %s\n", k, par_inst[k-i]);
+                            seq_args[k] = (char**) malloc(MAX_INST_SIZE * sizeof(char**));
+                            MEMCHECK(seq_args[k]);
+
+                            seq_path_str[k] = (char*)calloc(MAX_PATH_LEN * sizeof(char) , 1);
+                            MEMCHECK(seq_path_str[k]);
+
+                            seq_redirect_file[k] = (char*)malloc(MAX_FILE_LEN);
+                            MEMCHECK(seq_redirect_file[k]);
+
+                            check_cmd_ret = check_cmd(par_inst[k-i], seq_args[k], &seq_args_count[k], seq_path_str[k], seq_redirect_file[k], &seq_redirect[k]);
+                            multi_mode[k] = 0;
+                            if(check_cmd_ret == -1)
+                            {
+                                break;
+                            }
+                        }
+
+                        if(check_cmd_ret == -1)
+                        {
+                            throw_err();
+                            break;
+                        }
+                        
+                        /* Update counters */
+                        i = k -1;
+                        seq_cnt += par_cnt -1; 
+                    }
+                    else
+                    {
+                        seq_args[i] = (char**) malloc(MAX_INST_SIZE * sizeof(char**));
+                        MEMCHECK(seq_args[i]);
+
+                        seq_path_str[i] = (char*)calloc(MAX_PATH_LEN * sizeof(char) , 1);
+                        MEMCHECK(seq_path_str[i]);
+
+                        seq_redirect_file[i] = (char*)malloc(MAX_FILE_LEN);
+                        MEMCHECK(seq_redirect_file[i]);
+                
+                        //printf("-------------------------> Seq inst %d : %s\n", i, seq_inst[j]);
+                        check_cmd_ret = check_cmd(seq_inst[j], seq_args[i], &seq_args_count[i], seq_path_str[i], seq_redirect_file[i], &seq_redirect[i]);
+                        multi_mode[i] = 1; 
+                        if(check_cmd_ret == -1)
+                        {
+                            throw_err();
+                            break;
+                        }
+                    }
                 }
-                execute_par_inst(par_inst, parallel_cnt);
             }
 
-            /* Execute if syntax is correct */
-            if(check_cmd_ret == -1)
+            if(!is_multiline)
             {
-                //Invalid command throw error
-                throw_err();
+                /* Execute if syntax is correct */
+                if(check_cmd_ret == -1)
+                {
+                    //Invalid command throw error
+                    throw_err();
+                }
+                else if(check_cmd_ret == 0)
+                {
+                    //printf("Command success... Path count : %d\n", path_cnt);
+
+                    execute_ret = execute_cmd(args, args_count, path_str, redirect_file, redirect, 1);
+                     if(execute_ret<0)
+                     {
+                         throw_err();
+                        // break;
+                     }
+                }
             }
-            else if(check_cmd_ret == 0)
+            else
             {
-                printf("Command success... Path count : %d\n", path_cnt);
+                //printf("\nStarting execution: count = %d !!!!! \n\n", seq_cnt);
+                if(check_cmd_ret != -1)
+                {
+                    //printf("-------------------------------------------------\n");
+                     //printf("\n\nParsed seq inst :\n ");
+                    //int j;
 
-                                execute_ret = execute_cmd(args, args_count, path_str, redirect_file, redirect);
-                (void) execute_ret;
+                    /*
+                    for(i = 0; i < seq_cnt; i++)
+                    {
+                        for(j =0 ; j< seq_args_count[i]; j++)
+                        {
+                            printf("\t%s  %d \n", seq_args[i][j], multi_mode[i]);
+                        }
+                    }
+                    */
+
+                    //printf("-------------------------------------------------\n");
+                    for(i = 0; i < seq_cnt; i++)
+                    {
+                      //  printf("Executing inst : %d\n",  i);
+
+                        execute_ret = execute_cmd(seq_args[i], seq_args_count[i], seq_path_str[i], seq_redirect_file[i], seq_redirect[i], multi_mode[i]);
+
+                        prev_par_pid_cnt = par_pid_cnt;
+
+                        if(multi_mode[i] == 0) // parallel ops 
+                        {
+                            par_pid[par_pid_cnt++] = execute_ret;
+                        }
+                        else
+                        {
+                            par_pid_cnt = 0;
+                        }
+
+                        if(multi_mode[i] == 0)
+                        {
+                            
+                                waitpid(execute_ret,  NULL, 0);
+                        }
+
+                        ///printf("prev : %d , cur : %d\n", prev_par_pid_cnt, par_pid_cnt);
+
+                        if((prev_par_pid_cnt == 1 && par_pid_cnt == 0) || (i == seq_cnt - 1 && multi_mode[i] == 1)) 
+                        {
+                            for(k = 0; k < par_pid_cnt; k++)
+                            {
+                               // printf("waiting for pid ...\n");
+                                waitpid(par_pid[k], NULL, 0);
+                            }
+                            //:wq!
+                            (void)par_pid;
+                        }
+
+
+
+                    }
+                }
+
+                
+                for(i = 0; i < seq_cnt; i++)
+                {
+                    if(seq_args[i] != NULL)
+                        free(seq_args[i]);
+                    if(seq_path_str[i]!= NULL)
+                        free(seq_path_str[i]);
+                    if(seq_redirect_file[i]!= NULL)
+                        free(seq_redirect_file[i]);
+                }
             }
-
             free(inp_dup);
         }
     }
 
-    free(redirect_file);
 
     /* Free memory allocation */
+    free(seq_args);
+    free(seq_args_count);
+    free(seq_redirect_file);
+    free(seq_redirect);
+    free(seq_args);
+    free(seq_path_str);
+    free(seq_redirect_file);
+    free(redirect_file);
     free(path_str);
-
     for(i = 0; i < args_count; i++)
     {
         free(args[i]);
@@ -499,11 +636,40 @@ int main(int argc, char** argv)
         free(PATH[i]);
     }
     free(PATH);
-    for(i = 0 ; i < parallel_cnt; i++)
+    for(i = 0 ; i < seq_cnt; i++)
     {
-        free(par_inst[i]);
+        free(seq_inst[i]);
     }
     free(inp);
-    free(par_inst);
+    free(seq_inst);
     return 0;
+}
+
+
+int main(int argc, char** argv)
+{
+
+    FILE* fp ; 
+
+    if(argc == 2)
+    {
+        /* Batch mode */
+        fp = fopen(argv[1], "r");
+        if(fp == NULL)
+        {
+            throw_err();
+        }
+        shell(fp, 0);
+    }
+    else if(argc > 2)
+    {
+        throw_err();
+        exit(1);
+    }
+    else
+    {
+        shell(stdin, 1);
+    }
+
+
 }
